8a9f6038d212374b29088bc9a4ec63ed1838e0df
Nick Doiron <ndoiron@mapmeld.com>
2013-06-12 10:47:16 -0400

    making progress

--- vanishing.py
+++ vanishing.py
@@ -2,22 +2,24 @@
 # Python that controls an ArcGIS webmap
 
 # import essential JS objects
-map = JSObject(main_map);
-console = JSObject(console);
-esri = JSObject(esri);
+map = JSObject(main_map)
+console = JSObject(console)
+esri = JSObject(esri)
+Point = JSObject(pointmaker)
+PictureMarkerSymbol = JSObject(picturemaker)
+Graphic = JSObject(graphicmaker)
+SpatialReference = JSObject(srmaker)
 
 # re-center map
 map.centerAndZoom( [ -71, 42 ], 8 )
 
 # test creating a point, adding a marker
 
-pt = esri.geometry.Point( [-71, 42], esri.SpatialReference({ "wkid": 4326 } ) )
+pt = Point( [-71, 42], SpatialReference({ "wkid": 4326 } ) )
 
-symbol = esri.symbol.PictureMarkerSymbol( "marker.png", 40, 40 )
+symbol = PictureMarkerSymbol( "marker.png", 40, 40 )
 
-graphic = esri.Graphic( pt, symbol )
-
-map.graphics.add(graphic)
+map.graphics.add( Graphic( pt, symbol ) )
 
 # test output to console
 console.log( map )
