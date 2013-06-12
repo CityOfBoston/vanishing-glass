52f3209e0d189c42fa69e0a2d69b020133a9bd11
Nick Doiron <ndoiron@mapmeld.com>
2013-06-12 10:51:17 -0400

    testing constructors

--- vanishing.py
+++ vanishing.py
@@ -16,10 +16,8 @@ map.centerAndZoom( [ -71, 42 ], 8 )
 # test creating a point, adding a marker
 
 pt = Point( [-71, 42], SpatialReference({ "wkid": 4326 } ) )
-console.log(pt)
 
 symbol = PictureMarkerSymbol( "marker.png", 40, 40 )
-console.log(symbol)
 
 gr = Graphic( pt, symbol )
 console.log(gr)
