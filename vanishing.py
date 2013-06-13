d43387185168f9a5b189c5e989fcc15d2207fd49
Nick Doiron <ndoiron@mapmeld.com>
2013-06-13 10:29:47 -0400

    add js functions for working marker

--- vanishing.py
+++ vanishing.py
@@ -5,20 +5,23 @@
 main_map = JSObject(main_map)
 esri = JSObject(esri)
 
+SpatialReference = JSObject(wgs84)
+Point = JSObject(ptmaker)
+PictureMarkerSymbol = JSObject(picmaker)
+Graphic = JSObject(grmaker)
+
 # re-center map
 main_map.centerAndZoom( [ -71, 42 ], 8 )
 
 # test creating a point, adding a marker
 
-sr = esri.SpatialReference( { "wkid": 4326 } )
+pt = Point( [-71, 42], SpatialReference )
 
-pt = esri.geometry.Point( [-71, 42], sr )
+symbol = PictureMarkerSymbol( "marker.png", 40, 40 )
 
-symbol = esri.symbol.PictureMarkerSymbol( "marker.png", 40, 40 )
+gr = Graphic( pt, symbol )
 
-gr = esri.Graphic( pt, symbol )
-
-main_map.graphics.add( gr )
+main_map.graphics.add(gr)
 
 # test output to console
 JSObject(console).log(main_map)
